752dbcb1fda35708d526205744a582948797d94a
Enrique Alcántara <enrique_eduaa@ciencias.unam.mx>
2024-01-22 23:01:33 -0600

    Se hizo el primer prototipo de la vista de totems

---
+++ resources/views/totems/totem.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+        <title>Spidey-Pedia</title>
+    </head>
+
+    <body>
+        <div>
+            <p>Nombre:</p>
+            <p>Universo:</p>
+            <p>Alias:</p>
+            <p>Primera aparición:</p>
+            <p>Año de creación:</p>
+            <p>Poderes:</p>
+            <p>Historia:</p>
+            <p>Vivido:</p>
+        </div>
+    </body>
+
+</html>
