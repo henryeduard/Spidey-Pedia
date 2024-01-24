c2c959ee57446dc69f1dad93eff7f2c5616719bc
Enrique Alcántara <enrique_eduaa@ciencias.unam.mx>
2024-01-23 19:39:20 -0600

    Se creo una ruta y un metodo en el controlador para ver la vista individual de los totems

--- app/Http/Controllers/ControladorTotem.php
+++ app/Http/Controllers/ControladorTotem.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Totem;
 use Illuminate\Http\Request;
 
 class ControladorTotem extends Controller
 {
-    //
+    public function vistaIndividual($nombre){
+        $totem = Totem::where('nombre', $nombre)->first();
+
+        return view('totems.totem', [
+            'totem' => $totem,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ControladorTotem;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/spider-totem/{nombre}-{universo}', [ControladorTotem::class, 'vistaIndividual'])->name('vistaPlanPersonal');
