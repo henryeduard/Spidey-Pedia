0e2a867d5b7991b77f86cae0ccdd7fc3c05ab665
Enrique Alcántara <enrique_eduaa@ciencias.unam.mx>
2024-01-27 00:05:27 -0600

    Se ajusto el layoutMenu

--- resources/views/components/layoutMenu.blade.php
+++ resources/views/components/layoutMenu.blade.php
@@ -1,69 +1,68 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="es">
 
 <head>
+
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Spidey-Pedia</title>
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/alertify.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/validacion.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/jquery-ui.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/internos.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
-
+    {{-- <link rel="stylesheet" href="{{ URL::asset('css/alertify.min.css') }}"> --}}
+    {{-- <link rel="stylesheet" href="{{ URL::asset('css/validacion.css') }}"> --}}
+    {{-- <link rel="stylesheet" href="{{ URL::asset('css/jquery-ui.css') }}"> --}}
 
     {{ $linksCss }}
 
-    <script src="{{ URL::asset('js/jquery-3.6.4.min.js') }}"></script>
+    {{-- <script src="{{ URL::asset('js/jquery-3.6.4.min.js') }}"></script>
     <script src="{{ URL::asset('js/jquery.validate.min.js') }}"></script>
     <script src="{{ URL::asset('js/additional-methods.min.js') }}"></script>
     <script src="{{ URL::asset('js/messages_es.min.js') }}"></script>
     <script src="{{ URL::asset('js/localValidateMethods.js') }}"></script>
     <script src="{{ URL::asset('js/alertify.min.js') }}"></script>
     <script src="{{ URL::asset('js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ URL::asset('js/jquery-ui.min.js') }}"></script>
-
+    <script src="{{ URL::asset('js/jquery-ui.min.js') }}"></script> --}}
 
     {{ $JSscripts }}
+
 </head>
 
 <body class="bg-body-tertiary">
     <div class="container">
         <header>
-            Imagenes para header
+            {{-- Imagenes para header --}}
             <div class="row mb-1 mt-1">
                 <div class="col-2">
-                    <a href="https://www.icat.unam.mx/" target="_blank">
-                        <img src="{{ URL::asset('images/ICAT.png') }}" width="100%">
+                    <a href="#" target="_blank">
+                        <img src="{{ URL::asset('images/Spider-Man-logo.png') }}" width="100%">
                     </a>
                 </div>
-                <div class="col-2 offset-8 text-end">
+                {{-- <div class="col-2 offset-8 text-end">
                     <a href="https://www.unam.mx/" target="_blank">
                         <img src="{{ URL::asset('images/escudoUNAM.png') }}" width="40%">
                     </a>
-                </div>
+                </div> --}}
             </div>
         </header>
 
-        Se obtiene el usuario autentificado para verificar su rol
+        {{-- Se obtiene el usuario autentificado para verificar su rol --}}
         @php
-            $academico = auth()->user()->academico;
-            $fechaLimite = strtotime('2024-01-13 T00:00');
-            $thisMomento = strtotime('now');
-            $rol6 = auth()
-                ->user()
-                ->userRole->where('idRole', 6)
-                ->where('estado', 'A')
-                ->last();
+            $usuario = auth()->user();
+            // $rol6 = auth()
+            //     ->user()
+            //     ->userRole->where('idRole', 6)
+            //     ->where('estado', 'A')
+            //     ->last();
         @endphp
         
-        Menu superior
+        {{-- Menu superior --}}
         <x-menu>
             <x-items.academicoMenu></x-items.academicoMenu>
             <x-items.salirMenu></x-items.salirMenu>
         </x-menu>
 
-        <div class="row mt-1">
+        {{-- <div class="row mt-1">
             Menu lateral
             <div class="col-md-2">
                 <x-items.informePlanMenu></x-items.informePlanMenu>
@@ -81,34 +80,33 @@
                     {{ $body }}
                 @endif
             </div>
-        </div>
+        </div> --}}
 
         <footer class="bg-body-secondary mt-5 rounded-3 mx-auto mt-5">
             <div class="row justify-content-center align-items-center g-2 mt-5">
                 <div class="col-8 text-center offset-2">
-                    <p>Hecho en México, Universidad Nacional Autónoma de México (UNAM), todos
-                        los derechos
-                        reservados
-                        2021. Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se
-                        mutile,
-                        se cite la fuente completa y su dirección electrónica. De otra forma, requiere permiso
-                        previo
-                        por escrito de la institución.</p>
+                    <p>
+                        Hecho por Enrique Eduardo Alcántara Arce. 
+                        Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se
+                        mutile, se cite la fuente completa y su dirección electrónica. 
+                        De otra forma, requiere permiso previo
+                        por escrito de la institución.
+                    </p>
                 </div>
                 <div class="col-1 text-center offset-1">
                     <p class="float"><a href="#" title="Ir arriba"><i class="bi bi-arrow-up-circle-fill"
                                 style="font-size: 50px; color: #003D79;"></i></a></p>
                 </div>
             </div>
-            <div class="row justify-content-center align-items-center g-2 ">
+            {{-- <div class="row justify-content-center align-items-center g-2 ">
                 <div class="col-6 text-center">
                     <a class="nav-link" title="Avisos de privacidad" target="_blank" itemprop="url"
                         href="https://www.icat.unam.mx/avisos-privacidad/"><span itemprop="name">Avisos de
                             privacidad</span></a>
                 </div>
-            </div>
+            </div> --}}
         </footer>
     </div>
 </body>
 
-</html> --}}
+</html>
