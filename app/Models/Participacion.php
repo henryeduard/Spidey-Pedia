e9ebebc7bba4e4ad7fef9b619976544edad596b2
henryeduard <enrique_eduaa@ciencias.unam.mx>
2024-10-21 00:54:29 -0600

    Se creo el método tipoParticipacionToString

--- app/Models/Participacion.php
+++ app/Models/Participacion.php
@@ -8,4 +8,63 @@ use Illuminate\Database\Eloquent\Model;
 class Participacion extends Model
 {
     use HasFactory;
+
+    protected $table = "participaciones";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [];
+
+    // Devuelve el estado de tipoParticipacion en STRING
+    public function tipoParticipacionToString()
+    {
+        $estadoTipoParticipacion = $this->tipoParticipacion;
+        $stringTipoParticipacion = "";
+
+        switch($estadoTipoParticipacion) {
+            case 'Dib':
+                $stringTipoParticipacion = "Dibujante";
+                break;
+
+            case 'E':
+                $stringTipoParticipacion = "Escritor";
+                break;
+
+            case 'Dir':
+                $stringTipoParticipacion = "Director";
+                break;
+
+            case 'P':
+                $stringTipoParticipacion = "Productor";
+                break;
+
+            case 'De':
+                $stringTipoParticipacion = "Desarrollador";
+                break;
+
+            case 'A':
+                $stringTipoParticipacion = "Actor";
+                break;
+
+            case 'Av':
+                $stringTipoParticipacion = "Actor de voz";
+                break;
+
+            case 'Com':
+                $stringTipoParticipacion = "Compositor";
+                break;
+
+            case 'Col':
+                $stringTipoParticipacion = "Colorista";
+                break;
+            
+            default:
+                break;
+        }
+
+        return $stringTipoParticipacion;
+    }
 }
