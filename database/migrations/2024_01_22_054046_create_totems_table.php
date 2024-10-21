6756b9fbbeb6422e2c142170553b6a88267c57a3
henryeduard <enrique_eduaa@ciencias.unam.mx>
2024-10-20 21:51:46 -0600

    Nueva propuesta para migration de Totems

--- database/migrations/2024_01_22_054046_create_totems_table.php
+++ database/migrations/2024_01_22_054046_create_totems_table.php
@@ -13,29 +13,27 @@ return new class extends Migration
     {
         Schema::create('totems', function (Blueprint $table) {
             $table->id();
+
             $table->string('alias', 100)
                 ->comment("Nombre de Héroe");
             $table->string('nombre', 100)
                 ->comment("Nombre de civil");
             $table->string('apodo', 100)
                 ->comment("Nombre (no oficial) con el que se le conoce");
-            // $table->foreignId('idUniverso')
-            //     ->nullable()
-            //     ->constrained("universos")
-            //     ->restrictOnDelete()
-            //     ->cascadeOnUpdate();
-            $table->integer('universo');
-            $table->string('primeraAparicion', 250);
-            // $table->foreignId('primeraAparicion')
-            //     ->nullable()
-            //     ->constrained("media")
-            //     ->restrictOnDelete()
-            //     ->cascadeOnUpdate();
-            $table->integer('anioCreacion');
+            // $table->integer('universo');
+            $table->foreignId('idUniverso')
+                ->nullable()
+                ->constrained("universos")
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+            // $table->string('primeraAparicion', 250);
+            // $table->integer('anioCreacion');
             $table->text('poderes');
             $table->text('historia');
             $table->enum('vivido', ['V', 'M', 'D'])
                 ->comment("V: Vivo  -  M: Muerto - D: Desconocido");
+            $table->string('imagen', 50)->nullable();
+            
             $table->timestamps();
         });
     }
